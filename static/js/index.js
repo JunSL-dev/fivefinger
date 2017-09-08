955ea176de58d8055cc7ad75a135c33b55b318f9
이승준 <john_naker@mac.local>
2017-09-08 22:11:21 +0900

    Yo come back with my hope

--- static/js/index.js
+++ static/js/index.js
@@ -10,3 +10,40 @@ $(window).on('scroll',function(){
 		$('#hide_nav').fadeOut(100);
 	}
 });
+
+$(document).ready(function(){
+	
+	var scrollEvent = false;
+	var cnt = 0;
+
+	$('html,body').on('mousewheel',function(e){
+		e.preventDefault();
+
+		var m = e.originalEvent.wheelDelta;
+		var height = $('.uwi').height();
+		
+		if(m>1 && scrollEvent==false && cnt>=1){
+			console.log("up");
+			scrollEvent=true;
+			cnt--;
+			$('html, body').stop().animate({
+				scrollTop:height*cnt		
+			},{duration:1000, complete:function(){
+				scrollEvent = false;
+			}});
+		} else if(m<1 && scrollEvent==false && cnt <3){
+			console.log("down");
+			scrollEvnet=true;
+			cnt++;
+			$('html, body').stop().animate({
+				scrollTop:height*cnt		
+			},{duration:1000, complete:function(){
+				scrollEvent = false;
+			}});
+		}
+
+
+	});
+
+});
+
